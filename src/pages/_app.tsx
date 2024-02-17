20b9885a88487124cfe189c7c7b42cd19f2329e4
Sina Kheiry <m.sinakheiry.warwolf@gmail.com>
2024-02-17 22:27:10 +0330

    Added shadcn theming to project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
+import { ThemeProvider } from "@/components/utils/ThemeProvider";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+	return (
+		<ThemeProvider
+			attribute="class"
+			defaultTheme="system"
+			enableSystem
+			disableTransitionOnChange
+		>
+			<Component {...pageProps} />
+		</ThemeProvider>
+	);
 }
