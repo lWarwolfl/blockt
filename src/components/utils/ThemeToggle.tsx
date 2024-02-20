75c062a6a0b57edc9892d483653eb7957c928860
Sina Kheiry <m.sinakheiry.warwolf@gmail.com>
2024-02-20 14:32:00 +0330

    Project formating, Eslint and Prettier config, Icon pack config

--- src/components/utils/ThemeToggle.tsx
+++ src/components/utils/ThemeToggle.tsx
@@ -1,31 +1,45 @@
-import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
-
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
 import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+   DropdownMenu,
+   DropdownMenuContent,
+   DropdownMenuItem,
+   DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Icon } from '@iconify-icon/react'
+import { useTheme } from 'next-themes'
 
 export function ThemeToggle() {
-	const { setTheme } = useTheme();
+   const { setTheme } = useTheme()
 
-	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild>
-				<Button variant="outline" size="icon">
-					<Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-					<Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-					<span className="sr-only">Toggle theme</span>
-				</Button>
-			</DropdownMenuTrigger>
-			<DropdownMenuContent align="end">
-				<DropdownMenuItem onClick={() => setTheme("light")}>Light</DropdownMenuItem>
-				<DropdownMenuItem onClick={() => setTheme("dark")}>Dark</DropdownMenuItem>
-				<DropdownMenuItem onClick={() => setTheme("system")}>System</DropdownMenuItem>
-			</DropdownMenuContent>
-		</DropdownMenu>
-	);
+   return (
+      <DropdownMenu>
+         <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+               <Icon
+                  icon="line-md:moon-to-sunny-outline-loop-transition"
+                  className="absolute block text-xl dark:hidden"
+               />
+               <Icon
+                  icon="line-md:sunny-outline-to-moon-alt-loop-transition"
+                  className="absolute hidden text-xl dark:block"
+               />
+               <span className="sr-only">Toggle theme</span>
+            </Button>
+         </DropdownMenuTrigger>
+         <DropdownMenuContent align="start">
+            <DropdownMenuItem onClick={() => setTheme('light')} className="flex gap-2">
+               <Icon icon="line-md:sunny-outline-loop" className="text-xl" />
+               Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme('dark')} className="flex gap-2">
+               <Icon icon="line-md:moon-alt-loop" className="text-xl" />
+               Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme('system')} className="flex gap-2">
+               <Icon icon="ic:outline-memory" className="text-xl" />
+               System
+            </DropdownMenuItem>
+         </DropdownMenuContent>
+      </DropdownMenu>
+   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,11 @@
-import { ThemeProvider } from "@/components/utils/ThemeProvider";
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { ThemeProvider } from '@/components/utils/ThemeProvider'
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<ThemeProvider
-			attribute="class"
-			defaultTheme="system"
-			enableSystem
-			disableTransitionOnChange
-		>
-			<Component {...pageProps} />
-		</ThemeProvider>
-	);
+   return (
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+         <Component {...pageProps} />
+      </ThemeProvider>
+   )
 }
