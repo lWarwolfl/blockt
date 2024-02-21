593af81b2d0088eac4b92c017a5d099142b73a6b
Sina Kheiry <m.sinakheiry.warwolf@gmail.com>
2024-02-21 14:27:12 +0330

    Another test

--- src/components/utils/ThemeProvider.tsx
+++ src/components/utils/ThemeProvider.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { type ThemeProviderProps } from 'next-themes/dist/types'
+import { Poppins } from 'next/font/google'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
    return <NextThemesProvider {...props}>{children}</NextThemesProvider>
