c6940f8b36678734003824c4bc1f868f641a5eaa
Sina Kheiry <m.sinakheiry.warwolf@gmail.com>
2024-03-18 13:40:37 +0330

    Updated package

--- src/lib/web3/checkProvider.ts
+++ src/lib/web3/checkProvider.ts
@@ -1,10 +1,14 @@
 import type { MetaMaskInpageProvider } from '@metamask/providers'
+import toast from 'react-hot-toast'
 
 export const checkProvider = () => {
    const ethereum = global?.window?.ethereum
 
    const checkMetamask = () => {
-      if (!ethereum || !ethereum.isMetaMask) return undefined
+      if (!ethereum || !ethereum.isMetaMask) {
+         toast.error('MetaMask is not installed.')
+         return undefined
+      }
       return ethereum as unknown as MetaMaskInpageProvider
    }
 
